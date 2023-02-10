13ab586792ee904e085b5a7b67c522613472f0f1
Yacine667 <sid.yacinee@gmail.com>
2023-02-10 16:04:00 +0100

    utilisation api dans symfony pour afficher la liste des jeux en select et le detail d'un jeu en le selectionnant

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\HttpClient\BGAHttpClient;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -14,5 +17,17 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    #[Route('/games', name: 'app_games', methods: ['POST'])]
+    public function displayGames(BGAHttpClient $bga, Request $request) {
+        $search = $request->request->get('searchValue');
+        return new Response($bga->getGames($search));
+    }
+
+    #[Route('/game', name: 'app_game', methods: ['POST'])]
+    public function displayGame(BGAHttpClient $bga, Request $request) {
+        $search = $request->request->get('gameId');
+        return new Response($bga->getGame($search));
     }
 }
--- src/HttpClient/BGAHttpClient.php
+++ src/HttpClient/BGAHttpClient.php
@@ -1,15 +1,47 @@
 <?php
 
-// class HttpClient {
+namespace App\HttpClient;
 
-// public function getGame($search) {
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-//     $client_id = $this->getParameter('client_id');
-//     $response = $this->httpClient->request('GET',"/api/search?ids=$search&pretty=true&client_id=$client_id", [
-//         'verify_peer' => false,
-//     ]);
-//     return $response->getContent();
-// }
+class BGAHttpClient extends AbstractController {
 
-// }
+     /**
+      * @var HttpClientInterface
+      */
+
+     private $httpClient;
+
+     /**
+      * BGAHttpClient constructor.
+      *     
+      * @param HttpClientInterface $bga     
+      */
+
+    public function __construct(HttpClientInterface $bga)
+    {
+        $this->httpClient = $bga;
+    }
+
+    public function getGames($search){
+
+        $client_id = $this->getParameter('client_id');
+        $response = $this->httpClient->request('GET', "/api/search?name=$search&client_id=$client_id&limit=50", [
+            'verify_peer' => false, 
+        ]);
+        //dd($response->getContent());
+        return $response->getContent();       
+    }
+
+    public function getGame($search){
+
+        $client_id = $this->getParameter('client_id');
+        $response = $this->httpClient->request('GET', "/api/search?ids=$search&pretty=true&client_id=$client_id", [
+            'verify_peer' => false,
+        ]);
+        return $response->getContent();
+    }
+
+}
 ?>
