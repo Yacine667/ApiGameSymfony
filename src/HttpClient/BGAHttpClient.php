2deb65904b8b3c74a89e0abea1c214a191fb3154
Yacine667 <sid.yacinee@gmail.com>
2023-02-10 10:56:20 +0100

    Début Api Sur Symfony

---
+++ src/HttpClient/BGAHttpClient.php
@@ -0,0 +1,15 @@
+<?php
+
+// class HttpClient {
+
+// public function getGame($search) {
+
+//     $client_id = $this->getParameter('client_id');
+//     $response = $this->httpClient->request('GET',"/api/search?ids=$search&pretty=true&client_id=$client_id", [
+//         'verify_peer' => false,
+//     ]);
+//     return $response->getContent();
+// }
+
+// }
+?>
